1945df289a25495aaf7d689929fbea08e0dac3e5
patricewhite <patricewhite2012@gmail.com>
2017-06-22 12:30:57 -0400

    Working on gameform component

--- src/components/game.js
+++ src/components/game.js
@@ -5,14 +5,27 @@ import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
+export default class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      guessCount:0,
+      guessNumbers:[]
+    }
+  }
+
+  increment(){
+    this.setState({guessCount: this.state.guessCount + 1});
+  }
+
+  render(){
     return (
         <div>
             <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
+            <GuessSection feedback="Make your guess!"  onChange={() => this.increment()}/>
+            <GuessCount count={this.state.guessCount} />
             <GuessList guesses={[10, 15, 25]} />
         </div>
     );
+  }
 }
-
